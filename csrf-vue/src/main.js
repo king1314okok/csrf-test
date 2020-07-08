f5ebd24385ecd3af640427b52d62aff67d0afdc5
king1314okok <759046694@qq.com>
2020-07-08 20:28:28 +0800

    前端改vue-cli4

--- csrf-vue/src/main.js
+++ csrf-vue/src/main.js
@@ -1,34 +1,12 @@
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
 
-router.beforeEach((to, from, next) => {
-  /* 路由发生变化修改页面title */
-  if (to.meta.title) {
-    document.title = to.meta.title
-  }
-  const requireAuth = to.meta.requireAuth
-  // 判断该路由是否需要登录权限
-  if (requireAuth) {
-    if (window.sessionStorage.getItem('loginStatus')) {
-      next()
-    } else {
-      next('/login')
-    }
-  } else {
-    next()  // 确保一定要有next()被调用
-  }
-})
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- csrf-vue/src/router/index.js
+++ csrf-vue/src/router/index.js
@@ -1,29 +1,50 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import TableTest from '@/components/TableTest'
-import LoginPage from '@/components/Login'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  mode : 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'tableTest',
-      component: TableTest,
-      meta: {
-        title: '首页',
-        requireAuth: true
-      }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginPage,
-      meta: {
-        title: '登录'
-      }
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage,
+    meta: {
+      title: '登录'
     }
-  ]
+  }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  const requireAuth = to.meta.requireAuth
+  // 判断该路由是否需要登录权限
+  if (requireAuth) {
+    if (window.sessionStorage.getItem('loginStatus')) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()  // 确保一定要有next()被调用
+  }
+})
+
+export default router
